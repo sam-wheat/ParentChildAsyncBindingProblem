2bcad346d4eec177927302b3ffc79444eaceacb5
Sam Wheat <Sam.Wheat@alaskaair.com>
2024-09-19 14:29:38 -0700

    Initial commit

---
+++ ParentChildAsyncBindingProblemTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ParentChildAsyncBindingProblemTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Fail();
+        }
+    }
+}
